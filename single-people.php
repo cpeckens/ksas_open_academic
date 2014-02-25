715a4885fb45751fd612551a661686503cad1ed4
Cara Peckens <cpeckens@gmail.com>
2014-02-25 10:36:39 -0500

    Fix template call for job market

--- single-people.php
+++ single-people.php
@@ -2,10 +2,10 @@
 <div class="row sidebar_bg radius10" id="page">
 	<div class="nine columns wrapper radius-left offset-topgutter push-three">	
 		<section class="content">
-			<?php
-				if (has_term('faculty', 'role') == true ) {
-					locate_template('parts-faculty.php', true, false); } else {
-					locate_template('parts-jobmarket.php', true, false); } 
+			<?php 
+				if (has_term('', 'role') && !has_term('job-market-candidate', 'role')) {
+					get_template_part('parts', 'faculty'); } else {
+					get_template_part('parts', 'jobmarket'); } 
 			 ?>	
 		</section>
 	</div>	<!-- End main content (left) section -->
